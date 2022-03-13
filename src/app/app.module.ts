a5ef6806439d53747414f28ea060af0dc31be33d
Cristianoalves226 <c.alvesdecampos@gmail.com>
2022-03-13 13:40:37 -0400

    criação e alterações componentes star e courses

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CourseListComponent } from './courses/course-list.component';
+import { StarComponent } from './stars/star.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseListComponent,
+    StarComponent
   ],
   imports: [
-    BrowserModule
+  
+  BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
